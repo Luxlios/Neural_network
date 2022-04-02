8a1e0207a1d84a6b5f358a4ac14cd595ddb11b02
Luxlios <88307079+Luxlios@users.noreply.github.com>
2022-04-02 15:04:53 +0800

    Update E.py

--- VGG/E.py
+++ VGG/E.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
     loss_function = nn.CrossEntropyLoss()
     # GPU
     # device = torch.device("cuda:0")
-    # network = LeNet().to(device)
+    # network = vggE(n_classs=10).to(device)
     network = vggE(n_class=10)
     optimizer = optim.SGD(network.parameters(), lr=0.001, momentum=0.9)
 
@@ -137,7 +137,7 @@ if __name__ == '__main__':
             for data in iter(trainloader):
                 inputs, labels = data
     #             GPU
-    #             images, labels = images.to(device), labels.to(device)
+    #             inputs, labels = inputs.to(device), labels.to(device)
                 outputs = network(inputs)
                 probability, prediction = torch.max(outputs, 1)
                 total += labels.size(0)
@@ -152,7 +152,7 @@ if __name__ == '__main__':
             for data in iter(testloader):
                 inputs, labels = data
     #             GPU
-    #             images, labels = images.to(device), labels.to(device)
+    #             inputs, labels = inputs.to(device), labels.to(device)
                 outputs = network(inputs)
                 probability, prediction = torch.max(outputs, 1)
                 total += labels.size(0)
