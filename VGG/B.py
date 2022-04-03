25097100d05c23bd7fcd1b9f7adcee0168dd3631
Luxlios <88307079+Luxlios@users.noreply.github.com>
2022-04-03 19:33:18 +0800

    Update B.py

--- VGG/B.py
+++ VGG/B.py
@@ -54,8 +54,8 @@ class vggB(nn.Module):
             nn.ReLU(),
             nn.Linear(in_features=4096, out_features=1000),
             nn.ReLU(),
-            nn.Linear(in_features=1000, out_features=n_class)
-
+            nn.Linear(in_features=1000, out_features=n_class), 
+            nn.Softmax(dim=1)
         )
 
     def forward(self, x):
